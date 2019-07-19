bed497260f0afb7e9a6efe2d6b453dee6dec3083
smathis <smathis@vmware.com>
2019-07-19 16:57:36 -0500

    it's alive

--- src/clr-base/src/lib/components/simple-button/simple-button.ts
+++ src/clr-base/src/lib/components/simple-button/simple-button.ts
@@ -3,13 +3,70 @@
  * This software is released under MIT license.
  * The full license information can be found in LICENSE in the root directory of this project.
  */
-import cssVars from 'css-vars-ponyfill/dist/css-vars-ponyfill.esm.js';
+// import cssVars from 'css-vars-ponyfill/dist/css-vars-ponyfill.esm.js';
 
-interface ShadyWindow extends Window {
-  ShadyCSS: any;
-}
+import { LitElement, html, css } from 'lit-element';
+
+export const SIMPLE_BUTTON_TAG_NAME = 'simple-button';
 
 (function() {
+  class SimpleButton extends LitElement {
+    static get styles() {
+      return css`
+        :host {
+          all: initial;
+          display: block;
+          contain: content;
+          max-width: 500px;
+          margin: 0 auto;
+          border-radius: 8px;
+          transition: transform 0.2s ease-out;
+        }
+
+        :host([hidden]) {
+          display: none;
+        }
+
+        button,
+        span {
+          font-size: 1rem;
+          font-family: monospace;
+          padding: 0.25rem 0.5rem;
+        }
+
+        button {
+          cursor: pointer;
+          background: var(--global-button-color, red);
+          color: var(--global-button-font-color, black);
+          border: 0;
+          border-radius: 6px;
+          box-shadow: 0 0 5px rgba(173, 61, 85, 0.5);
+        }
+
+        button:active {
+          background: #ad3d55;
+          color: white;
+        }
+
+        .bright {
+          color: #ffa500;
+          font-size: 0.75rem;
+          margin-bottom: 0.25rem;
+        }
+      `;
+    }
+
+    render() {
+      return html`
+        <span class="bright">My LIT ELEMENT lives here</span>
+        <button type="button">This is a button</button>
+        `;
+    }
+  }
+
+  window.customElements.define(SIMPLE_BUTTON_TAG_NAME, SimpleButton);
+
+  /*
   const template = document.createElement('template');
 
   template.innerHTML = `
@@ -97,6 +154,5 @@ interface ShadyWindow extends Window {
       }
     }
   }
-
-  window.customElements.define(componentTagName, MyElement);
+  */
 })();
--- src/dev/src/polyfills.ts
+++ src/dev/src/polyfills.ts
@@ -8,23 +8,6 @@
  * BROWSER POLYFILLS
  */
 
-// V these polyfills were missing for some reason. i probably deleted them like an idoit
-/* IE9, IE10 and IE11 requires all of the following polyfills. */
-// import 'core-js/es6/symbol';
-// import 'core-js/es6/object';
-// import 'core-js/es6/function';
-// import 'core-js/es6/parse-int';
-// import 'core-js/es6/parse-float';
-// import 'core-js/es6/number';
-// import 'core-js/es6/math';
-// import 'core-js/es6/string';
-// import 'core-js/es6/date';
-// import 'core-js/es6/array';
-// import 'core-js/es6/regexp';
-// import 'core-js/es6/map';
-// import 'core-js/es6/weak-map';
-// import 'core-js/es6/set';
-
 // https://stackoverflow.com/questions/54764616/problem-with-angular-element-support-in-chrome-and-ie11-simultaneously
 // ...said to include this before the polyfills listed below.
 import 'core-js/shim';
@@ -36,71 +19,8 @@ import 'core-js/shim';
 import 'core-js/es6/reflect';
 
 /* Polyfill for icons web components */
-
-// V this polyfill fixed most of the problems
-// this shim throws a syntax error because there is an arrow function () => {} in it...
-// ¿ maybe there is an es5 compiled version?
-// !!! this is the current issue i'm struggling with
-// import '@webcomponents/custom-elements/src/native-shim.js';
-
-// vv this polyfill breaks using HTMLelement as a constructor
-// combined with custom-elements below gives me an "Object or property does not support attach shadow" in IE11
-// ...but! the app builds in ie11. it doesn't navigate or anything tho
-// we need this polyfill to make custom elements show up in Chrome on the devapp!
-// import '@webcomponents/webcomponentsjs/custom-elements-es5-adapter.js';
-
-// This was the recommended bundle but is causes the same "defineProperty"
-// console error as the bundle up above (which makes sense, tbh)
-// import '@webcomponents/webcomponentsjs/bundles/webcomponents-sd-ce-pf.js';
-
-// V this polyfill was no help. cannot find "defineProperty"
-// import '@webcomponents/webcomponentsjs/webcomponents-bundle.js';
-
-// vv i added this polyfill back in;
-// all alone it gives me an "Object or property does not support attach shadow" in IE11
+// once we move icons to Lit this will no longer be needed
 // import '@webcomponents/custom-elements';
-// import '@webcomponents/shadydom';
-// import '@webcomponents/shadycss';
-
-// this vvv was recommended but doesn't exist. maybe their dox are out of date?
-// import '@webcomponents/webcomponentsjs/webcomponents-loader.js';
-
-// according to... https://stackoverflow.com/questions/54764616/problem-with-angular-element-support-in-chrome-and-ie11-simultaneously
-// these polyfills need to be in THIS order....
-// import '@webcomponents/shadydom';
-// import '@webcomponents/custom-elements/src/native-shim';
-// import '@webcomponents/custom-elements';
-
-/*
-// following Cory's attempt we landed here...
-import '@webcomponents/shadydom';
-import '@webcomponents/shadycss/scoping-shim.min.js';
-// import '@webcomponents/shadycss/apply-shim.min.js';
-// import '@webcomponents/shadycss/custom-style-interface.min.js';
-import '@webcomponents/template';
-import '@webcomponents/webcomponentsjs/custom-elements-es5-adapter.js';
-import '@webcomponents/custom-elements';
-*/
-
-// 2019-07-17: This is what was recommended. But it fails because ngCli
-// is moving the webcomponents files somewhere that the loader cannot find them
-// i tried using glob in angular.json to no avail because it appears these files
-// are getting munged up in webpack.
-// 2019-07-17: I am now trying these in scripts of angular.json per https://stackoverflow.com/questions/56511672/angular-application-v8-not-working-with-web-component-polyfills-in-ie11
-// 2019-07-17: that didn't work at
-// import '@webcomponents/webcomponentsjs/custom-elements-es5-adapter.js';
-// import '@webcomponents/webcomponentsjs/webcomponents-loader.js';
-
-// 2019-07-17: these worked great actually. a bit of a sledgehammer approach but everything showed up as expected.
-import '@webcomponents/webcomponentsjs/custom-elements-es5-adapter.js';
-import '@webcomponents/webcomponentsjs/bundles/webcomponents-sd-ce-pf.js';
-
-// 2019-07-17: loading the ponyfill here passes "undefined" as the global object
-// should we try loading this in scripts?! <= didn't work. same problem.
-// import 'css-vars-ponyfill/dist/css-vars-ponyfill.js';
-
-// 2019-07-17: trying to load as a module in the clr-base?!
-// import 'css-vars-ponyfill/dist/css-vars-ponyfill.js';
 
 /*
  * Required to support Web Animations `@angular/animation`.
