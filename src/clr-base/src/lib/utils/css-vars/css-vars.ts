7be8049b577fc6b538734dca13b56a2a68d4dde4
smathis <smathis@vmware.com>
2019-07-31 13:54:01 -0500

    clean up

--- src/clr-base/src/lib/utils/css-vars/css-vars.ts
+++ src/clr-base/src/lib/utils/css-vars/css-vars.ts
@@ -3,39 +3,11 @@
  * This software is released under MIT license.
  * The full license information can be found in LICENSE in the root directory of this project.
  */
-
-import IWindow from '../../interfaces/window.interface';
 import { cssVars, variableStore } from './css-vars-ponyfill-fork.esm.js';
 
-declare var window: IWindow;
-
-function hasCssVars(win: IWindow): boolean {
-  return win.hasOwnProperty('cssVars');
-}
-
-// exporting so we can test it
-export function cssVarsPolyfillHasRun(win: IWindow): boolean {
-  const defaultPolyfillObject = { polyfills: { cssVarsHasRun: false } };
-
-  // TODO: needs a new name
-  if (!win.hasOwnProperty('__ClarityInternals')) {
-    win.__ClarityInternals = { ...defaultPolyfillObject };
-    return false;
-  } else if (!win.__ClarityInternals.hasOwnProperty('polyfills')) {
-    Object.assign(win.__ClarityInternals, { ...defaultPolyfillObject });
-    return false;
-  } else if (typeof win.__ClarityInternals.polyfills.cssVarsHasRun === 'undefined') {
-    win.__ClarityInternals.polyfills.cssVarsHasRun = false;
-    return false;
-  } else {
-    return !!win.__ClarityInternals.polyfills.cssVarsHasRun;
-  }
-}
-
 export function runCssVarsPolyfill(config?: any): void {
-  const runCssVarsPolyfillStart = new Date();
-  if (typeof window !== 'undefined' && hasCssVars(window)) {
-    window.cssVars = cssVars;
+  if (typeof window !== 'undefined') {
+    (window as any).cssVars = cssVars;
   }
 
   if (typeof config === 'undefined') {
@@ -57,30 +29,11 @@ export function runCssVarsPolyfill(config?: any): void {
       //   console.log('css vars on success');
       // },
       onComplete: function(cssText) {
-        // window.__ClarityInternals.polyfills.cssVarsHasRun = true;
-        // console.log('css vars ran with default config - on complete');
-        // console.log(variableStore.dom);
-        const runCssVarsPolyfillEnd = new Date();
-        let shadyCssStyleDocumentEnd: Date;
-        console.log(
-          'cssVars took ',
-          runCssVarsPolyfillEnd.valueOf() - runCssVarsPolyfillStart.valueOf(),
-          ' milliseconds'
-        );
         (window as any).ShadyCSS.styleDocument(variableStore.dom);
-        shadyCssStyleDocumentEnd = new Date();
-        console.log(
-          'shadyCssStyleDocument in cssVars onComplete took ',
-          shadyCssStyleDocumentEnd.valueOf() - runCssVarsPolyfillEnd.valueOf(),
-          ' milliseconds'
-        );
       },
     };
   }
 
-  // if (!cssVarsPolyfillHasRun(window)) {
-  //   cssVars(config);
-  // }
   cssVars(config);
 }
 
